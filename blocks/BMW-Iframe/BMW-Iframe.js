add5193b178f99196f3c7b77fa6aa29496a844d0
Amol <amol.y.patil@accenyure.com>
2024-02-22 19:48:50 +0530

    changed values to dynamic
    changed values to dynamic

--- blocks/BMW-Iframe/BMW-Iframe.js
+++ blocks/BMW-Iframe/BMW-Iframe.js
@@ -1,12 +1,12 @@
-export function generateIFrameDOM() {
+export function generateIFrameDOM(props) {
   // Extract properties, always same order as in model, empty string if not set
-  // const [iFrameUrl, iframeHeight, iframeWidth] = props;
+  const [iFrameUrl, iframeHeight, iframeWidth] = props;
   // const picture = pictureContainer.querySelector('picture');
 
   // Build DOM
   const iFrameDOM = document.createRange().createContextualFragment(`
     <div>
-    <iframe src="https://feature-script-for-adobe-data-layer--x-walk--harish-deva.hlx.page" width="1000" height="1000" style="border:1px solid black;">
+    <iframe src="${iFrameUrl.innerHTML}" width="${iframeWidth.innerHTML}" height="${iframeHeight.innerHTML}" style="border:1px solid black;">
     </iframe>
     
     </div>
@@ -16,8 +16,8 @@ export function generateIFrameDOM() {
 
 export default function decorate(block) {
   // get the first and only cell from each row
-  // const props = [...block.children].map((row) => row.firstElementChild);
-  const iFrameDOM = generateIFrameDOM();
+  const props = [...block.children].map((row) => row.firstElementChild);
+  const iFrameDOM = generateIFrameDOM(props);
   block.textContent = '';
   block.append(iFrameDOM);
 }
