dd47e8acaa9f7aa9f4b31c25d74b032a1e09d5d4
Amol <amol.y.patil@accenyure.com>
2024-02-23 13:55:09 +0530

    Updated js file to dynamic values
    Updated js file to dynamic values

--- blocks/bmw-iframe/bmw-iframe.js
+++ blocks/bmw-iframe/bmw-iframe.js
@@ -1,6 +1,6 @@
-export function generateIFrameDOM() {
+export function generateIFrameDOM(props) {
   // Extract properties, always same order as in model, empty string if not set
-  // const [iFrameUrl, iframeHeight, iframeWidth] = props;
+  const [iFrameUrl, iframeHeight, iframeWidth] = props;
   // const picture = pictureContainer.querySelector('picture');
   // Build DOM
   const iFrameDOM = document.createRange().createContextualFragment(`
@@ -15,8 +15,8 @@ export function generateIFrameDOM() {
 
 export default function decorate(block) {
   // get the first and only cell from each row
-  // const props = [...block.children].map((row) => row.firstElementChild);
-  const iFrameDOM = generateIFrameDOM();
+  const props = [...block.children].map((row) => row.firstElementChild);
+  const iFrameDOM = generateIFrameDOM(props);
   block.textContent = '';
   block.append(iFrameDOM);
 }
