1522f9ad9787a899fda1bc5784a8316fed4f2d54
Harish <harish396@gmail.com>
2024-03-13 12:08:33 +0100

    npm alloy script included

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -118,13 +118,14 @@ function launchVariables() {
       window.adobeDataLayer.push({ event: 'Configurator Start', foo: 'bar', key: 'value' });
     }
   }
-    // Check if the click was a link
-    const anchor = document.getElementById("feature-frame").src;
-    if (!anchor) return;
-    const url = new URL(anchor);
-    if (!url.hostname.endsWith("feature-test-frame--x-walk--harish-deva.hlx.page/component-test-page")) return;
+  // Check if the click was a link
 
-    alloy("appendIdentityToUrl", {url: anchor}).then(result => {document.location = anchor;});
+  const anchor = document.getElementById('feature-frame').src;
+  if (!anchor) return;
+  const url = new URL(anchor);
+  if (!url.hostname.endsWith('feature-test-frame--x-walk--harish-deva.hlx.page/component-test-page')) return;
+
+  alloy('appendIdentityToUrl', { url: anchor });
 }
 
 async function loadPage() {
